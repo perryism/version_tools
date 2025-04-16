a6451641a6ea0a5c8cefaa845b2ce4a306bddaaf
Perry Lee <malee@taskrabbit.com>
2025-04-16 12:32:47 -0700

    cli.get supports yaml

--- version_tools/cli/get.py
+++ version_tools/cli/get.py
@@ -2,10 +2,17 @@ from glom import glom
 import argparse
 import sys
 import json
+import yaml
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('key', help='json path')
+argparser.add_argument('--yaml', action='store_true', help='parse yaml')
 
 args = argparser.parse_args()
 
-print(glom(json.load(sys.stdin), args.key))
+if args.yaml:
+    data = yaml.safe_load(sys.stdin)
+else:
+    data = json.load(sys.stdin)
+
+print(glom(data, args.key))
